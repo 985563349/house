9837e80347523da3bbc9f0deb33571357f245285
王杰 <jie985563349@outlook.com>
2023-09-25 17:45:40 +0800

    update tinaCMS editor

--- src/pages/demo/posts/[filename].tsx
+++ src/pages/demo/posts/[filename].tsx
@@ -2,11 +2,18 @@
 // This is a demo file once you have tina setup feel free to delete this file
 
 import Head from 'next/head';
-import { useTina } from 'tinacms/dist/react';
+import { tinaField, useTina } from 'tinacms/dist/react';
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
-import client from '../../../../tina/__generated__/client';
+import client from '@/tina/__generated__/client';
+import type { PostQuery } from '@/tina/__generated__/types';
 
-const BlogPage = (props) => {
+const BlogPage = (props: {
+  data: PostQuery;
+  variables: {
+    relativePath: string;
+  };
+  query: string;
+}) => {
   const { data } = useTina({
     query: props.query,
     variables: props.variables,
@@ -25,17 +32,35 @@ const BlogPage = (props) => {
           referrerPolicy="no-referrer"
         />
       </Head>
+
       <div>
         <div
           style={{
             textAlign: 'center',
           }}
         >
-          <h1 className="text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <h1
+            className="text-3xl m-8 text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+            data-tina-field={tinaField(data.post, 'title')}
+          >
             {data.post.title}
           </h1>
-          <ContentSection content={data.post.body}></ContentSection>
+
+          <div className="relative py-16 bg-white overflow-hidden">
+            <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+              <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true"></div>
+            </div>
+            <div className="relative px-4 sm:px-6 lg:px-8">
+              <div
+                data-tina-field={tinaField(data.post, 'body')}
+                className="text-lg max-w-prose mx-auto"
+              >
+                <TinaMarkdown content={data.post.body} />
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className="bg-green-100 text-center">
           Lost and looking for a place to start?
           <a
@@ -52,7 +77,7 @@ const BlogPage = (props) => {
   );
 };
 
-export const getStaticProps = async ({ params }) => {
+export const getStaticProps = async ({ params }: { params: { filename: string } }) => {
   let data = {};
   let query = {};
   let variables = { relativePath: `${params.filename}.mdx` };
@@ -79,124 +104,11 @@ export const getStaticPaths = async () => {
   const postsListData = await client.queries.postConnection();
 
   return {
-    paths: postsListData.data.postConnection.edges.map((post) => ({
-      params: { filename: post.node._sys.filename },
+    paths: postsListData.data.postConnection.edges?.map((post) => ({
+      params: { filename: post?.node?._sys.filename },
     })),
     fallback: false,
   };
 };
 
 export default BlogPage;
-
-const PageSection = (props) => {
-  return (
-    <>
-      <h2>{props.heading}</h2>
-      <p>{props.content}</p>
-    </>
-  );
-};
-
-const components = {
-  PageSection: PageSection,
-};
-
-const ContentSection = ({ content }) => {
-  return (
-    <div className="relative py-16 bg-white overflow-hidden">
-      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-        <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
-          <svg
-            className="absolute top-12 left-full transform translate-x-32"
-            width={404}
-            height={384}
-            fill="none"
-            viewBox="0 0 404 384"
-          >
-            <defs>
-              <pattern
-                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect width={404} height={384} fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
-          </svg>
-          <svg
-            className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
-            width={404}
-            height={384}
-            fill="none"
-            viewBox="0 0 404 384"
-          >
-            <defs>
-              <pattern
-                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect width={404} height={384} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
-          </svg>
-          <svg
-            className="absolute bottom-12 left-full transform translate-x-32"
-            width={404}
-            height={384}
-            fill="none"
-            viewBox="0 0 404 384"
-          >
-            <defs>
-              <pattern
-                id="d3eb07ae-5182-43e6-857d-35c643af9034"
-                x={0}
-                y={0}
-                width={20}
-                height={20}
-                patternUnits="userSpaceOnUse"
-              >
-                <rect
-                  x={0}
-                  y={0}
-                  width={4}
-                  height={4}
-                  className="text-gray-200"
-                  fill="currentColor"
-                />
-              </pattern>
-            </defs>
-            <rect width={404} height={384} fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
-          </svg>
-        </div>
-      </div>
-      <div className="relative px-4 sm:px-6 lg:px-8">
-        <div className="text-lg max-w-prose mx-auto">
-          <TinaMarkdown components={components} content={content} />
-        </div>
-      </div>
-    </div>
-  );
-};
