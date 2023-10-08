7d5c24e8803edbb43815bd4b3edeb6e8bd378167
王杰 <jie985563349@outlook.com>
2023-10-08 12:23:36 +0800

    Create CC BY-NC-SA 4.0

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,16 @@ const Footer: React.FC = () => {
   return (
     <footer className="flex justify-center">
       <div className="flex items-center px-6 w-full h-20 max-w-[1024px]">
-        <p className="text-sm">This blog is built with NestJS.</p>
+        <p className="text-sm opacity-50">
+          <a
+            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            target="_blank"
+            className="mr-1 underline"
+          >
+            CC BY-NC-SA 4.0
+          </a>
+          2023-PRESENT © Jee
+        </p>
       </div>
     </footer>
   );
