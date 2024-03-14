f88bc4f4a7be7bf9cf88875d7f3b56afd69ab547
王杰 <jie985563349@outlook.com>
2024-03-14 18:42:51 +0800

    code block line highlight

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,4 +1,12 @@
 import { Code } from 'bright';
+import type { Extension } from 'bright';
+
+const highlight: Extension = {
+  name: 'highlight',
+  MultilineAnnotation: ({ children }) => {
+    return <span className="block bg-white bg-opacity-5">{children}</span>;
+  },
+};
 
 export type CodeBlockProps = {
   lang?: string;
@@ -6,7 +14,11 @@ export type CodeBlockProps = {
 };
 
 const CodeBlock = (props?: CodeBlockProps) => {
-  return <Code lang={props?.lang}>{props?.value}</Code>;
+  return (
+    <Code lang={props?.lang} theme="material-palenight" extensions={[highlight]}>
+      {props?.value}
+    </Code>
+  );
 };
 
 export { CodeBlock };
