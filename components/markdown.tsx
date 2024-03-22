10e45a0ce0eb9fbc7c9690b26f30588fb88dd444
王杰 <jie985563349@outlook.com>
2024-03-22 11:36:59 +0800

    optimize markdown component

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,30 +1,27 @@
 import { TinaMarkdown } from 'tinacms/dist/rich-text';
 
-import { StackBlitzEmbed } from '@/components/stack-blitz-embed';
-import { GitHubGistEmbed } from '@/components/github-gist-embed';
-import { CodeBlock } from '@/components/code-block';
 import { CodeSandboxEmbed } from '@/components/code-sandbox-embed';
+import { CodeBlock } from '@/components/code-block';
+import { GitHubGistEmbed } from '@/components/github-gist-embed';
+import { StackBlitzEmbed } from '@/components/stack-blitz-embed';
 
-export type MarkdownProps = React.ComponentProps<typeof TinaMarkdown>;
+export type TinaMarkdownProps = React.ComponentProps<typeof TinaMarkdown>;
+export type MarkdownProps = Omit<TinaMarkdownProps, 'components'>;
 
-const Markdown: React.FC<MarkdownProps> = ({ components, ...props }) => {
-  return (
-    <TinaMarkdown
-      {...props}
-      components={{
-        CodeSandboxEmbed,
-        StackBlitzEmbed,
-        GitHubGistEmbed,
-        code_block: CodeBlock,
-        a: (props) => (
-          <a href={props?.url} target="_blank">
-            {props?.children}
-          </a>
-        ),
-        ...components,
-      }}
-    />
-  );
+const components = {
+  CodeSandboxEmbed,
+  GitHubGistEmbed,
+  StackBlitzEmbed,
+  code_block: CodeBlock,
+  a: (props) => (
+    <a href={props?.url} target="_blank">
+      {props?.children}
+    </a>
+  ),
+} as TinaMarkdownProps['components'];
+
+const Markdown: React.FC<MarkdownProps> = ({ ...props }) => {
+  return <TinaMarkdown {...props} components={components} />;
 };
 
 export { Markdown };
