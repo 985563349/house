d9eb4f0e74845a1589071a1f16d792c64cac23ae
Jee <jie985563349@outlook.com>
2025-04-06 16:53:15 +0800

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,7 @@ export default function Home() {
     <div className="flex items-center justify-center mx-auto px-8 py-10 max-w-screen-lg min-h-full">
       <section>
         <div className="slide-enter-content flex flex-col items-center gap-6 mx-auto max-w-3xl text-center">
-          <Image
-            className="rounded-full"
-            src="/avatar.png"
-            alt="avatar"
-            width={100}
-            height={100}
-          />
+          <Image className="rounded-full" src="/avatar.png" alt="avatar" width={100} height={100} />
           <TypeAnimation
             className="min-h-7 text-xl"
             wrapper="p"
@@ -21,10 +15,8 @@ export default function Home() {
             speed={20}
             sequence={['嗨，我是 Jee 👋🏻']}
           />
-          <p className="text-4xl font-semibold">
-            一个 Web 开发者 · 终身学习者 · 游戏爱好者
-          </p>
-          <p>我喜欢学习新的技术和框架，目前正在寻找新的工作机会 👨‍💻。</p>
+          <p className="text-4xl font-semibold">一个 Web 开发者 · 终身学习者 · 游戏爱好者</p>
+          <p>我喜欢学习新的技术和框架，目前正在一家科技公司担任前端工程师 👨‍💻。</p>
         </div>
       </section>
     </div>
